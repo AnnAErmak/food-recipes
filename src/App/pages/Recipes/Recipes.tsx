53b1ff29da0b419b52938eb4f8991be335fee3af
Ermak Anna <ermak-anna86@yandex.ru>
2025-03-30 12:15:33 +0300

    add image

--- src/App/pages/Recipes/Recipes.tsx
+++ src/App/pages/Recipes/Recipes.tsx
@@ -1,6 +1,9 @@
 import React, {useEffect} from "react";
 import {getRecipes} from "../../../utils/getRecipes.ts";
 import {Link} from "react-router-dom";
+import cn from "classnames";
+import styles from "./Recipes.module.scss";
+import Text from "../../../components/Text";
 
 const Recipes: React.FC = () => {
 
@@ -11,7 +14,10 @@ const Recipes: React.FC = () => {
 
     return (
         <>
-            <div>Страница с рецептами</div>
+            <div className={cn(styles.imageContainer)} />
+            <Text view={'p-20'} color={'primary'} className={styles.text}>
+                Find the perfect food and drink ideas for every occasion, from weeknight dinners to holiday feasts.
+            </Text>
             <div style={{display: "flex", flexDirection:"column"}}>
                 {recipes.map((recipe, index) => (
                     <Link to={`/recipe/${recipe.id}`} key={index}>{recipe.id}</Link>))
